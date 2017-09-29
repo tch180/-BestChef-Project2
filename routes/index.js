3542b4f9504778364f0ec15662b9eb1c8c6e303a
tch180 <thopstadredner@gmail.com>
2017-09-29 12:32:10 -0400

    working on db for users, added views for user and recipes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Best CHEF' });
+  console.log('hello index')
+});
+router.get('/users', function(req, res, next) {
+  res.render('');
+  console.log('hello users')
 });
 
 
