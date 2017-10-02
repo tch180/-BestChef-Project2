77c336f3a5fc927c780bdf23bfeb6794e0dfa552
tch180 <thopstadredner@gmail.com>
2017-10-02 15:46:21 -0400

    fixed routes for users, still fleshing out some routes.

--- routes/userscontroller.js
+++ routes/userscontroller.js
@@ -9,35 +9,35 @@ const methodoverride = require('method-override');
 
 
 // INDEX GET users listing. 
-router.get('/', (request, response)=> {
+router.get('/', (request, response) => {
   registeredusers.find({})
-  .then((registeredusers)=>{
-    response.render('users/index', {
-      registeredusers: registeredusers
+    .then((registeredusers) => {
+      response.render('users/index', {
+        registeredusers: registeredusers
+      })
     })
-  })
-.catch((error)=>{
-  console.log('error')
-})
+    .catch((error) => {
+      console.log('error')
+    })
 }),
 
 
-//NEW ROUTE 
-router.get('/new', (req, res)=>{
-  res.render('users/new')
-});
+  //NEW ROUTE 
+  router.get('/new', (req, res) => {
+    res.render('users/new')
+  });
 
 
 //CREATE ROUTE
-router.post('/', (req,res)=>{
- const newUser = req.body
- registeredusers.create(newUser)
- .then(()=>{
-   res.redirect('/users/index')
- })
- .catch((error)=>{
-   res.send(error)
- })
+router.post('/', (req, res) => {
+  const newUser = req.body
+  registeredusers.create(newUser)
+    .then(() => {
+      res.redirect("/users")
+    })
+    .catch((error) => {
+      res.send(error)
+    })
 })
 
 // EDIT 
@@ -46,8 +46,8 @@ router.get('/:userId/edit', (req, res) => {
   registeredusers.findById(userId)
     .then((user) => {
       res.render('users/edit', {
-        user: user,
-        userId: userId
+        user: user
+
       })
     })
     .catch((error) => {
@@ -56,43 +56,47 @@ router.get('/:userId/edit', (req, res) => {
 })
 
 //UPDATE ROUTE 
- router.put('/:userId', (req, res)=>{
+router.put('/:userId', (req, res) => {
   const userId = req.params.userId
   const updatedUser = req.body
-  registeredusers.findByIdAndUpdate(userId, updatedUser, {new: true})
-  .then(()=>{
-    res.redirect(`/users/${userId}`)
+  registeredusers.findByIdAndUpdate(userId, updatedUser, {
+    new: true
   })
+    .then(() => {
+      res.redirect(`/users/${userId}`)
+      userId: userId
+    })
 })
- 
+
 
 // SHOW route 
-router.get('/:userId', (req, res)=>{
+router.get('/:userId', (req, res) => {
   const userId = req.params.userId
-registeredusers.findByIdAndUpdate(userId)
-  .then((user)=>{
-    res.render('users/edit')
-user: user
-userId: userId
-  })
+  registeredusers.findById(userId)
+    .then((user) => {
+      res.render('users/show')
+      user: user
 
-.catch((error) =>{
-  console.log(error)
-})
+    })
+
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 
 //Delete
-router.get('/:userId/delete', (req, res)=>{
+router.get('/:userId/delete', (req, res) => {
 
   const userId = req.params.userId
-  console.log('Route hit with userId of : ' + userId)
+
+
   registeredusers.findByIdAndRemove(userId)
-  .then((users)=>{
-    res.redirect('/users')
-  })
-  
-  })
+    .then((users) => {
+      res.redirect('/users')
+    })
+
+})
 
 
 module.exports = router;
