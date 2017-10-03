657004b4bcfeedacecde1783e70bb997de621fe3
tch180 <thopstadredner@gmail.com>
2017-10-03 11:52:20 -0400

    finished ingredients for recipes and linking it the views page, project is mvp, working on styling

--- db/schema.js
+++ db/schema.js
@@ -1,49 +1,45 @@
  //requirements: require mongoose
  const mongoose = require('mongoose')
- 
+
  //create your recipe schema:
  const Schema = mongoose.Schema
 
 
- const ingredSchema = new Schema(
-     {
-        name: String,
-        description: String,
-        prepTime: Number
-     }
- )
+ const ingredSchema = new Schema({
+     name: String,
+     description: String,
+     prepTime: Number,
+ })
 
- const recipeSchema = new Schema(
-     {
-            name: String,
-            description: String,
-            directions: String,
-            image:  String,
-            time: Number,
-         recipe:[ingredSchema]   
+ const recipeSchema = new Schema({
+     name: String,
+     description: String,
+     directions: String,
+     image: String,
+     time: Number,
+     ingredients: [ingredSchema]
 
-        })
+ })
 
-const registeredusersSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        
-        email: {
-            type: String,
-            required: true
-        },
-        
-})    
-  const recipeModel = mongoose.model('recipe', recipeSchema);
-  const ingredModel = mongoose.model('ingredients', ingredSchema);
-  const registeredusersModel = mongoose.model('users', registeredusersSchema);
+ const registeredusersSchema = new Schema({
+     name: {
+         type: String,
+         required: true,
+     },
+
+     email: {
+         type: String,
+         required: true
+     },
+
+ })
+ const recipeModel = mongoose.model('recipe', recipeSchema);
+ const ingredModel = mongoose.model('ingredients', ingredSchema);
+ const registeredusersModel = mongoose.model('users', registeredusersSchema);
  //export your recipe with module.exports()
-    
- module.exports = { 
+
+ module.exports = {
      recipeModel: recipeModel,
      ingredModel: ingredModel,
-     registeredusersModel: registeredusersModel, 
+     registeredusersModel: registeredusersModel,
  }
--- db/seeds.js
+++ db/seeds.js
@@ -20,7 +20,7 @@ db.once('open', () => {
 
 const recipeModel = schema.recipeModel;
 const ingredModel = schema.ingredModel;
-const registeredusersModel = schema.registeredusersModel; 
+const registeredusersModel = schema.registeredusersModel;
 
 
 
@@ -30,55 +30,51 @@ const Marsala = new recipeModel({
     image: "https://i.imgur.com/UIywQQI.jpg?1",
     origin: "Italy",
     time: 25,
-    directions: "Minim fugiat enim sit pariatur ex dolor ad quis consequat nulla esse dolor occaecat. Nostrud qui sit incididunt veniam ea quis fugiat cillum elit cillum commodo nisi occaecat. Elit enim dolor enim aliqua amet adipisicing."
+    directions: "Minim fugiat enim sit pariatur ex dolor ad quis consequat nulla esse dolor occaecat. Nostrud qui sit incididunt veniam ea quis fugiat cillum elit cillum commodo nisi occaecat. Elit enim dolor enim aliqua amet adipisicing.",
+    ingredients: [ingredModel],
+    
 });
 const Greenbean = new recipeModel({
     name: "Greenbean Alamdine",
     description: "Sauteed greenbeans",
-    image: "https://i.imgur.com/UIywQQI.jpg?1",
+    image: "https://i.imgur.com/IPnOsnu.jpg",
     origin: "Italy",
     time: 25,
-    directions: "Minim fugiat enim sit pariatur ex dolor ad quis consequat nulla esse dolor occaecat. Nostrud qui sit incididunt veniam ea quis fugiat cillum elit cillum commodo nisi occaecat. Elit enim dolor enim aliqua amet adipisicing."
+    directions: "Minim fugiat enim sit pariatur ex dolor ad quis consequat nulla esse dolor occaecat. Nostrud qui sit incididunt veniam ea quis fugiat cillum elit cillum commodo nisi occaecat. Elit enim dolor enim aliqua amet adipisicing.",
+    ingredients: [ingredModel]
 });
 const redpotato = new recipeModel({
     name: "Garlic roasted RedBliss potatoes",
     description: "roasted potatoes",
-    img: "#",
+    image: "https://i.imgur.com/tQ3DxIA.jpg",
     origin: "Italy",
     time: 1.5,
     directions: "Labore adipisicing mollit reprehenderit enim ea consequat labore nisi.",
+    ingredients: [ingredModel]
 });
 const MeatLoaf = new recipeModel({
     name: "Asian MeatLoaf",
     description: "Asian inspired meatloaf",
-    img: "#",
+    image: "https://i.imgur.com/UZdXmqk.jpg",
     origin: "fusion food",
     time: 2.5,
     directions: "Ut et elit ipsum est ex eu amet exercitation id ut eiusmod minim officia nisi.",
-
+    ingredients: [ingredModel]  
 });
-const recipes = [Marsala, Greenbean, redpotato, MeatLoaf]
 
-recipes.forEach((recipe) => {
-    recipe.save()
-        .then((recipe) => {
-            console.log(`${recipe.name} saved `)
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-})
+
 
 const chickenBreast = new ingredModel({
     name: "Chicken Breast",
     description: "Farm Raised NoN GMO and RBGH-1 free ",
     prepTime: 25,
-    img: 'https://i.imgur.com/UIywQQI.jpg?1'
+
 });
 const heavyCream = new ingredModel({
     name: "Heavy Whipping Cream",
     description: "Ultra Pasturized Heavy Cream, Great for cooking and sauces ",
     prepTime: 5,
+    
 });
 const marsalaWine = new ingredModel({
     name: "Marsala Wine",
@@ -101,23 +97,38 @@ const potato = new ingredModel({
     prepTime: 25,
 });
 
+const recipes = [Marsala, Greenbean, redpotato, MeatLoaf]
 const recipesingred = [chickenBreast, heavyCream, marsalaWine, garlic, beans, potato]
 
-recipesingred.forEach((recipesingred) => {
-    recipesingred.save()
-        .then((recipesingred) => {
-            console.log(`${recipesingred.name} saved `)
+recipes.forEach((recipe) => {
+    recipe.ingredients = recipesingred
+    recipe.save()
+        .then((recipe) => {
+            console.log(`${recipe.name} saved `)
         })
         .catch((error) => {
             console.log(error)
         });
-});
+})
+
+
+/* const recipesingred = [chickenBreast, heavyCream, marsalaWine, garlic, beans, potato]
+ */
+// recipesingred.forEach((recipesingred) => {
+//     recipesingred.save()
+//         .then((recipesingred) => {
+//             console.log(`${recipesingred.name} saved `)
+//         })
+//         .catch((error) => {
+//             console.log(error)
+//         });
+// });
 
 
 
 const bob = new registeredusersModel({
     name: "bob, Smith",
-    
+
     email: "BobSmith@gmail.com",
 });
 const Victory = new registeredusersModel({
@@ -134,23 +145,23 @@ const Elias = new registeredusersModel({
 const registeredusers = [bob, Victory, Elias]
 
 registeredusers.forEach((registeredusers) => {
-            registeredusers.save()
-                .then((registeredusers) => {
-                    console.log(`${registeredusers.name} saved `)
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
+    registeredusers.save()
+        .then((registeredusers) => {
+            console.log(`${registeredusers.name} saved `)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 
-            })
-
-
-           
+})
 
 
 
 
-     
+
+
+
+
 /* 
   recipe.create(newRecipe, function(err) {
         console.log("SEED: NEW PRODUCTS CREATED!");
@@ -162,5 +173,5 @@ registeredusers.forEach((registeredusers) => {
 //======================
 // EXPORTS
 //====================== */
-            /* module.exports = router; */
-            db.close();
+/* module.exports = router; */
+db.close();
